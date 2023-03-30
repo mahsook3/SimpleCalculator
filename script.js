a674c90e9ad6f9af260d74edbd6e51921d78a0c5
Mohamed Shaik Mahsook M A <101459627+mahsook3@users.noreply.github.com>
2023-03-30 22:02:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,40 @@ function clearAll() {
 }
 
 function calculate() {
-  let result = eval(calculation);
-  document.getElementById('result').value = result;
+  let result;
+  
+  // Fixing the decimal point bug
+  if (calculation.slice(-1) === '.') {
+    calculation = calculation.slice(0, -1);
+  }
+  
+  try {
+    result = eval(calculation);
+    document.getElementById('result').value = result;
+  } catch {
+    document.getElementById('result').value = 'Error';
+  }
+  
   calculation = '';
 }
+
+// Keyboard support
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    calculate();
+  } else if (event.key === 'Escape') {
+    clearAll();
+  } else if (event.key === '/' || event.key === '*' || event.key === '-' || event.key === '+') {
+    insert(event.key);
+  } else if (event.key === '.') {
+    // Fixing the decimal point bug
+    if (!calculation.includes('.')) {
+      insert(event.key);
+    }
+  } else if (event.key === 'Backspace') {
+    calculation = calculation.slice(0, -1);
+    document.getElementById('result').value = calculation;
+  } else if (event.key >= '0' && event.key <= '9') {
+    insert(event.key);
+  }
+});
